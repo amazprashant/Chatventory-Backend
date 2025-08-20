53be5f25cca03b0e78b57e3899daa804dc079e00
amazprashant <amazprashant@gmail.com>
2025-08-20 10:05:45 +0530

    update the files

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const port =  4000;
+const pool = require('./db');
+const dotenv = require('dotenv');
+
+const port = process.env.PORT;
 
 app.get('/', (req, res)=> {
     res.send('Hello World!');
+});
+
+app.get("/check-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ status: "Connected ✅", time: result.rows[0].now });
+  } catch (err) {
+    res.status(500).json({ status: "Failed ❌", error: err.message });
+  }
 });
 
 app.listen(port, () =>{
